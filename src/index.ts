35588a1a03d8f2e0c163aeabb4d18b39c99beb31
cod1r <jasonho353@gmail.com>
2022-04-15 00:49:38 -0500

    added radar and added a delay to it so it looks more realistic, made triangles more acute, added coordinates on screen, and added an offset to the coordinates so that the client's triangle stays on screen even if their actual coords go off screen and their offsetted coords stay correct relative to the other peers

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,74 @@ import { io } from "socket.io-client";
 import Peer from "simple-peer";
 import type { Instance as PeerType } from "simple-peer";
 
+// Declarations
+class Triangle {
+	ctx: CanvasRenderingContext2D = null;
+	location: [number, number] = null;
+	facing_angle: number = null;
+	constructor(ctx: CanvasRenderingContext2D, location: [number, number]) {
+		this.ctx = ctx;
+		this.location = location;
+		this.facing_angle = Math.PI / 2;
+	}
+	updateLocation = function(newLocation: [number, number]) {
+		this.location = newLocation;
+	}
+	updateFacingAngle = function(facing_angle: number) {
+		this.facing_angle = facing_angle;
+	}
+	draw = function() {
+		let x_loc = this.location[0];
+		let y_loc = this.location[1];
+		this.ctx.beginPath();
+		this.ctx.moveTo(x_loc + globalOffset[0], y_loc + globalOffset[1]);
+		this.ctx.fillText(`${Math.floor(x_loc)} ${Math.floor(y_loc)}`, x_loc + globalOffset[0], y_loc + globalOffset[1] - 20);
+		this.ctx.lineTo(
+			x_loc + -50 * Math.cos(this.facing_angle + Math.PI / 2 + 75 * Math.PI / 180) + globalOffset[0], 
+			y_loc + -50 * Math.sin(this.facing_angle + Math.PI / 2 + 75 * Math.PI / 180) + globalOffset[1]
+		);
+		this.ctx.lineTo(
+			x_loc + -50 * Math.cos(this.facing_angle + Math.PI / 2 + 75 * Math.PI / 180 + Math.PI / 6) + globalOffset[0], 
+			y_loc + -50 * Math.sin(this.facing_angle + Math.PI / 2 + 75 * Math.PI / 180 + Math.PI / 6) + globalOffset[1]
+		);
+		this.ctx.lineTo(
+			x_loc + globalOffset[0],
+			y_loc + globalOffset[1]
+		);
+		this.ctx.fill();
+		this.ctx.stroke();
+	}
+}
+
+
 let socket = io("https://tcas-signaling-server.herokuapp.com/");
 // let socket = io("http://192.168.1.252:3001");
 
 let peerConnections: Map<string, {connected: boolean, peer: PeerType}> = new Map();
 let otherPeerLocations: Map<string, {x: number, y: number, facing_angle: number}> = new Map();
+let canvas: HTMLElement | HTMLCanvasElement | null = document.getElementById("canvas");
+let radar: HTMLElement | HTMLCanvasElement | null = document.getElementById("radar");
+let mapSize: [number, number] = [1500, 1500];
+let ctx = (canvas as HTMLCanvasElement).getContext("2d");
+let radarCtx = (radar as HTMLCanvasElement).getContext("2d");
+let prevDate: number = Date.now();
+let distanceToEdge = 50;
+let tri: Triangle = new Triangle(
+	ctx, 
+	[
+		Math.floor(Math.random() * mapSize[0] - mapSize[0] / 2), 
+		Math.floor(Math.random() * mapSize[1] - mapSize[1] / 2)
+	]
+);
+let keysPressed: Set<string> = new Set();
+let globalOffset: [number, number] = [
+	(canvas as HTMLCanvasElement).width - tri.location[0], 
+	(canvas as HTMLCanvasElement).height - tri.location[1]
+];
+// console.log(tri.location);
+// console.log(globalOffset);
 
+// Socket and Peer things
 socket.on("pjoin", (...args) => {
 	// console.log("somebody joined");
 	let peer: PeerType = new Peer({ initiator: true, trickle: false });
@@ -26,6 +88,10 @@ socket.on("pjoin", (...args) => {
 	peer.on("data", data => {
 		//console.log("data received", data.toString());
 		otherPeerLocations.set(peerConnectionID, JSON.parse(data.toString()));
+	});
+	peer.on("close", () => {
+		otherPeerLocations.delete(peerConnectionID);
+		peerConnections.delete(peerConnectionID);
 	});
 	peer.on('error', err => console.log('error', err))
 	peerConnections.set(peerConnectionID, {connected: false, peer: peer});
@@ -52,56 +118,18 @@ socket.on("preply", (...args) => {
 			//console.log("data received", data.toString());
 			otherPeerLocations.set(peerConnectionID, JSON.parse(data.toString()));
 		});
+		peer.on("close", () => {
+			otherPeerLocations.delete(peerConnectionID);
+			peerConnections.delete(peerConnectionID);
+		});
 		peer.on('error', err => console.log('error', err))
 		peerConnections.set(peerConnectionID, {connected: false, peer: peer});
 	}
 });
 
 // Canvas Rendering Part
-let canvas: HTMLElement | HTMLCanvasElement | null = document.getElementById("canvas");
-let ctx = (canvas as HTMLCanvasElement).getContext("2d");
-
 (canvas as HTMLCanvasElement).width = window.innerWidth;
 (canvas as HTMLCanvasElement).height = window.innerHeight;
-
-class Triangle {
-	ctx: CanvasRenderingContext2D = null;
-	location: [number, number] = null;
-	facing_angle: number = null;
-	constructor(ctx: CanvasRenderingContext2D, location: [number, number]) {
-		this.ctx = ctx;
-		this.location = location;
-		this.facing_angle = Math.PI / 2;
-	}
-	updateLocation = function(newLocation: [number, number]) {
-		this.location = newLocation;
-	}
-	updateFacingAngle = function(facing_angle: number) {
-		this.facing_angle = facing_angle;
-	}
-	draw = function() {
-		let x_loc = this.location[0];
-		let y_loc = this.location[1];
-		this.ctx.beginPath();
-		this.ctx.moveTo(x_loc, y_loc);
-		this.ctx.lineTo(
-			x_loc + -50 * Math.cos(this.facing_angle + Math.PI / 2 + Math.PI / 3), 
-			y_loc + -50 * Math.sin(this.facing_angle + Math.PI / 2 + Math.PI / 3)
-		);
-		this.ctx.lineTo(
-			x_loc + -50 * Math.cos(this.facing_angle + Math.PI / 2 + Math.PI / 3 + Math.PI / 3), 
-			y_loc + -50 * Math.sin(this.facing_angle + Math.PI / 2 + Math.PI / 3 + Math.PI / 3)
-		);
-		this.ctx.lineTo(
-			x_loc,
-			y_loc
-		);
-		this.ctx.stroke();
-	}
-}
-
-let tri: Triangle = new Triangle(ctx, [100, 100]);
-let keysPressed: Set<string> = new Set();
 
 window.onkeydown = function (e) {
 	switch (e.key) {
@@ -135,7 +163,7 @@ window.onkeyup = function (e) {
 	}
 }
 
-async function sendInfo() {
+function sendInfo() {
 	for (const [peerConnectionID, { connected, peer }] of peerConnections) {
 		if (connected) {
 			peer.send(JSON.stringify({
@@ -150,21 +178,50 @@ async function sendInfo() {
 function drawOtherPeers() {
 	ctx.beginPath();
 	for (const [peerConnectionID, {x, y, facing_angle}] of otherPeerLocations) {
-		ctx.moveTo(x, y);
+		ctx.moveTo(x + globalOffset[0], y + globalOffset[1]);
+		ctx.fillText(`${Math.floor(x)} ${Math.floor(y)}`, x + globalOffset[0], y + globalOffset[1] - 20);
 		ctx.lineTo(
-			x + -50 * Math.cos(facing_angle + Math.PI / 2 + Math.PI / 3),
-			y + -50 * Math.sin(facing_angle + Math.PI / 2 + Math.PI / 3)
+			x + -50 * Math.cos(facing_angle + Math.PI / 2 + 75 * Math.PI / 180) + globalOffset[0],
+			y + -50 * Math.sin(facing_angle + Math.PI / 2 + 75 * Math.PI / 180) + globalOffset[1]
 		);
 		ctx.lineTo(
-			x + -50 * Math.cos(facing_angle + Math.PI / 2 + Math.PI / 3 + Math.PI / 3),
-			y + -50 * Math.sin(facing_angle + Math.PI / 2 + Math.PI / 3 + Math.PI / 3)
+			x + -50 * Math.cos(facing_angle + Math.PI / 2 + 75 * Math.PI / 180 + Math.PI / 6) + globalOffset[0],
+			y + -50 * Math.sin(facing_angle + Math.PI / 2 + 75 * Math.PI / 180 + Math.PI / 6) + globalOffset[1]
 		);
 		ctx.lineTo(
-			x,
-			y
+			x + globalOffset[0],
+			y + globalOffset[1]
 		);
 	}
 	ctx.stroke();
+}
+
+function drawRadar() {
+	radarCtx.clearRect(0, 0, (radar as HTMLCanvasElement).width, (radar as HTMLCanvasElement).height);
+	radarCtx.beginPath();
+	radarCtx.arc(
+		(radar as HTMLCanvasElement).width / 2, 
+		(radar as HTMLCanvasElement).height / 2, 
+		2, 
+		0, 
+		2*Math.PI
+	);
+	radarCtx.fill();
+	for (const [peerConnectionID, {x, y, facing_angle}] of otherPeerLocations) {
+		if (Math.sqrt((x - tri.location[0])**2 + (y - tri.location[1])**2) <= 800) {
+			radarCtx.beginPath();
+			radarCtx.arc(
+				(radar as HTMLCanvasElement).width / 2 + (x - tri.location[0]) * 250 / 1500, 
+				(radar as HTMLCanvasElement).height / 2 + (y - tri.location[1]) * 250 / 1500, 
+				2, 
+				0, 
+				2*Math.PI
+			);
+			radarCtx.fill();
+			radarCtx.stroke();
+		}
+	}
+	radarCtx.stroke();
 }
 
 function render() {
@@ -181,9 +238,28 @@ function render() {
 	if (keysPressed.has("ArrowRight")) {
 		tri.updateFacingAngle(tri.facing_angle + 1 / (2 * Math.PI));
 	}
+
+	if (tri.location[0] + globalOffset[0] <= distanceToEdge) {
+		globalOffset[0] = distanceToEdge - tri.location[0];
+	}
+	else if ((canvas as HTMLCanvasElement).width - distanceToEdge <= tri.location[0] + globalOffset[0]) {
+		globalOffset[0] = ((canvas as HTMLCanvasElement).width - distanceToEdge) - tri.location[0];
+	}
+
+	if (tri.location[1] + globalOffset[1] <= distanceToEdge) {
+		globalOffset[1] = distanceToEdge - tri.location[1];
+	}
+	else if ((canvas as HTMLCanvasElement).height - distanceToEdge <= tri.location[1] + globalOffset[1]) {
+		globalOffset[1] = ((canvas as HTMLCanvasElement).height - distanceToEdge) - tri.location[1];
+	}
+
 	sendInfo();
 	tri.draw();
 	drawOtherPeers();
+	if (Date.now() - prevDate >= 1000) {
+		drawRadar();
+		prevDate = Date.now();
+	}
 	requestAnimationFrame(render);
 }
 
